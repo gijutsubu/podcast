d39e6044b496ed2279640f983f02a62eb2fa4c76
nas <nas.silver.bullet@gmail.com>
2020-09-25 15:42:01 +0900

    :+1: fix date format

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,12 @@
-import { parseISO, format } from 'date-fns'
+import { format } from 'date-fns'
+import { zonedTimeToUtc } from 'date-fns-tz'
+
+
+type week = "日" | "月" | "火" | "水" | "木" | "金" | "土"
+
+const weeks: Array<week> = ["日", "月", "火", "水", "木", "金", "土"]
 
 export const Date = ({ raw }: { raw: string }) => {
-  const date = parseISO(raw)
-  return <time dateTime={raw}>{format(date, 'yyyy-mm-dd')}</time>
+  const date = zonedTimeToUtc(raw, 'Asia/Tokyo')
+  return <time dateTime={raw}>{format(date, 'yyyy年MM月dd日(') + weeks[date.getDay()] + ')'}</time>
 }
--- next.config.js
+++ next.config.js
@@ -13,5 +13,4 @@ module.exports = {
 
     return config;
   },
-  distDir: "build",
 };
