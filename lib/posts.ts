f85e024f1de4467748906a176bd9f720d1f4a3b8
nas <nas.silver.bullet@gmail.com>
2020-11-01 13:30:18 +0900

    fix dev posts

--- lib/posts.ts
+++ lib/posts.ts
@@ -7,6 +7,7 @@ import highlight from 'remark-highlight.js'
 import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
+const isDev = process.env.NODE_ENV === 'development'
 
 export type matterData = {
   title: string
@@ -16,43 +17,26 @@ export type matterData = {
   audiences: string[]
 }
 
-const getPostMetaData = (fileName: string) => {
-  const slug = fileName.replace(/\.md$/, '')
-
-  const fullPath = path.join(postsDirectory, fileName)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-  const matterResult = matter(fileContents)
-
-  const data = matterResult.data as matterData
-
-  return {
-    slug,
-    ...(data)
-  }
-}
-
-export const getSortedPostsData = () => {
-  const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map(getPostMetaData).filter(data => !data.draft)
-
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1
-    } else {
-      return -1
-    }
-  })
-}
-
 export type params = {
   slug: string
 }
 
 export const getAllPostIDs = () => {
   const fileNames = fs.readdirSync(postsDirectory)
-  return fileNames.map(getPostMetaData).filter(data => !data.draft).map(data => {
+  return allPostsData(fileNames).map(data => {
     return { params: { slug: data.slug } }
+  })
+}
+
+export const getSortedPostsData = () => {
+  const fileNames = fs.readdirSync(postsDirectory)
+
+  return allPostsData(fileNames).sort((a, b) => {
+    if (a.date < b.date) {
+      return 1
+    } else {
+      return -1
+    }
   })
 }
 
@@ -72,5 +56,24 @@ export const getPostData = async (slug: string) => {
     slug,
     contentHtml,
     ...matterResult.data
+  }
+}
+
+
+const allPostsData = (fileNames: string[]) => fileNames.map(getPostMetaData).filter(data => (isDev || !data.draft))
+
+const getPostMetaData = (fileName: string) => {
+  const slug = fileName.replace(/\.md$/, '')
+
+  const fullPath = path.join(postsDirectory, fileName)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+  const matterResult = matter(fileContents)
+
+  const data = matterResult.data as matterData
+
+  return {
+    slug,
+    ...(data)
   }
 }
